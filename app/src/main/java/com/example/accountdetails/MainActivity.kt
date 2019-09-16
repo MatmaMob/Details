a6272377080696f56bf120ca20d448c0e6710742
tomaszmatusik <tomasz.matusik95@gmail.com>
2019-09-16 18:04:21 +0700

    card detail view

--- app/src/main/java/com/example/accountdetails/MainActivity.kt
+++ app/src/main/java/com/example/accountdetails/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.accountdetails
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.view_card_details.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Glide.with(this)
+            .load("https://cd.foundation/wp-content/uploads/sites/78/2019/04/blank_portrait.png")
+            .into(profilePicture)
     }
 }
